066e524834fce166a3e228031df34cc9c5fcf295
kamalagj <kamalagj@code.edu.az>
2022-08-25 12:45:14 +0400

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,8 +23,57 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+ 
+function quickbox(){
+  if ($(window).width() > 767) {
+   $('.quickview-button').magnificPopup({
+   type:'iframe',
+   delegate: 'a',
+   preloader: true,
+   tLoading: 'Loading image #%curr%...',
+   });    
+  }  
+}
+jQuery(document).ready(function() {quickbox();});
+jQuery(window).resize(function() {quickbox();});
+$('input[name=\'search\']').autocomplete({
+ 'source': function(request, response) {
+   $.ajax({
+     url: 'index.php?route=product/search/autocomplete&filter_name=' +  encodeURIComponent(request),
+     dataType: 'json',
+     success: function(result) {
+       var products = result.products;
+       $('.ajaxishi-search ul li').remove();
+         $.each(products, function(index,product) {
+         var html = '<li>';
+           html += '<div>';
+           html += '<a href="' + product.url + '" title="' + product.name + '">';
+           html += '<div class="product-image"><img alt="' + product.name + '" src="' + product.image + '"></div>';
+           html += '<div class="product-desc">';
+           html += '<div class="product-name">' + product.name + '</div>';
+           if (product.special) {
+                           html += '<div class="product-price"><span class="special">' + product.price + '</span><span class="price">' + product.special + '</span></div>';
+                       } else {
+                           html += '<div class="product-price"><span class="price">' + product.price + '</span></div>';
+           }
+           html += '</div>';
+           html += '</a>';
+           html += '</div>';
+           html += '</li>';
+           $('.ajaxishi-search ul').append(html);
+         });
+         $('.ajaxishi-search').css('display','block');
+               return false;
+     }
+   });
+ },
+ 'select': function(product) {
+   $('input[name=\'filter_name\']').val(product.name);
+ }
+});
 
-$('.users').users({
+
+$('.owl-carousel').owlCarousel({
   loop:true,
   margin:10,
   responsiveClass:true,
@@ -34,13 +83,17 @@ $('.users').users({
           nav:true
       },
       600:{
-          items:2,
+          items:3,
           nav:false
       },
       1000:{
-          items:2,
+          items:5,
           nav:true,
           loop:false
       }
   }
 })
+
+
+
+
