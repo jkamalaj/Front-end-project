76982be376bb6af1514958ad208b4ef6b17d09a6
kamalagj <kamalagj@code.edu.az>
2022-08-25 21:15:44 +0400

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,76 +24,180 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
  
-function quickbox(){
-  if ($(window).width() > 767) {
-   $('.quickview-button').magnificPopup({
-   type:'iframe',
-   delegate: 'a',
-   preloader: true,
-   tLoading: 'Loading image #%curr%...',
-   });    
-  }  
-}
-jQuery(document).ready(function() {quickbox();});
-jQuery(window).resize(function() {quickbox();});
-$('input[name=\'search\']').autocomplete({
- 'source': function(request, response) {
-   $.ajax({
-     url: 'index.php?route=product/search/autocomplete&filter_name=' +  encodeURIComponent(request),
-     dataType: 'json',
-     success: function(result) {
-       var products = result.products;
-       $('.ajaxishi-search ul li').remove();
-         $.each(products, function(index,product) {
-         var html = '<li>';
-           html += '<div>';
-           html += '<a href="' + product.url + '" title="' + product.name + '">';
-           html += '<div class="product-image"><img alt="' + product.name + '" src="' + product.image + '"></div>';
-           html += '<div class="product-desc">';
-           html += '<div class="product-name">' + product.name + '</div>';
-           if (product.special) {
-                           html += '<div class="product-price"><span class="special">' + product.price + '</span><span class="price">' + product.special + '</span></div>';
-                       } else {
-                           html += '<div class="product-price"><span class="price">' + product.price + '</span></div>';
-           }
-           html += '</div>';
-           html += '</a>';
-           html += '</div>';
-           html += '</li>';
-           $('.ajaxishi-search ul').append(html);
-         });
-         $('.ajaxishi-search').css('display','block');
-               return false;
-     }
-   });
- },
- 'select': function(product) {
-   $('input[name=\'filter_name\']').val(product.name);
- }
-});
 
 
 $('.owl-carousel').owlCarousel({
+  stagePadding: 50,
   loop:true,
-  margin:10,
-  responsiveClass:true,
+  margin:0,
+  nav:true,
   responsive:{
       0:{
-          items:1,
-          nav:true
+          items:1
       },
       600:{
-          items:3,
-          nav:false
+          items:1
+      },
+      800:{
+          items: 2
       },
       1000:{
-          items:5,
-          nav:true,
-          loop:false
-      }
+          items:2
+      },
+    1200:{
+        items: 2
+    }
   }
 })
 
+let buttons =document.querySelectorAll(".addCart")
+let count=document.querySelector(".count")
+let table = document.querySelector("table")
+let tBody = document.querySelector("tbody")
+let tableZone = document.querySelector(".basket-not-empty")
+let empty = document.querySelector(".empty")
 
+let basket;
+let result=localStorage.getItem("basket");
+if (!result) {
+    localStorage.setItem("basket",JSON.stringify([]))
+}
+buttons.forEach((button)=>{
+    button.addEventListener("click",function(){
+        basket=JSON.parse(localStorage.getItem("basket")) || [];
+        let imgs=this.parentElement.parentElement.parentElement.querySelector("img").getAttribute("src").split("/")
+        let img=imgs[imgs.length-1]
+        let model=this.parentElement.parentElement.parentElement.getAttribute("data-id")
+        let name=this.parentElement.parentElement.querySelector(".name").innerText
+        let price=this.parentElement.parentElement.parentElement.querySelector(".price").innerText
+        let existed=basket.find((item)=>{
+           return item.id == id
+        })
+            
+        if(!existed){
+            let item={model,img,name,price,count:1}
+            basket.push(item)
+        }
+        else{
+            existed.count++;
+        }
+        localStorage.setItem("basket",JSON.stringify(basket))
+        if (count) {
+            GetCount()
+        }
+        AddToCard()
+    })
+})
+if (countArea) {
+    GetCount()
+}
+function GetCount() {
+    let CountBasket=JSON.parse(localStorage.getItem("basket")) || []
+    let count=CountBasket.reduce((total,val)=>{
+       return total+=val.count;
+    },0)
+    countArea.innerText=count
+    console.log(count);
+}
+function GetPrice() {
+    let Basket=JSON.parse(localStorage.getItem("basket")) || []
+    let totalPrice=Basket.reduce((total,val)=>{
+        return total+=val.count * val.price;
+     },0)
+     priceArea.innerText=totalPrice;
+}
+function AddToCard() {
 
+        
+        let basket=JSON.parse(localStorage.getItem("basket"))
 
+        basket.forEach((obj)=>{
+
+            let tr=document.createElement("tr")
+            let td1=document.createElement("td")
+            let td2=document.createElement("td")
+            let image=document.createElement("img")
+            image.style.width="100%"
+            let td3=document.createElement("td")
+            let td4=document.createElement("td")
+            let tdCount=document.createElement("td")
+            let tdId=document.createElement("td")
+            tdId.innerText=obj.id
+            tdId.id="id"
+            tdCount.innerText=obj.count
+            let btn=document.createElement("button")
+            btn.className="btn btn-danger"
+            btn.innerText="X"
+            image.src=`./assets/img/${obj.img}`
+            td1.append(image)
+            td2.innerText=obj.name
+            td3.innerText=obj.price
+            td4.append(btn)
+            tr.append(td1)
+            tr.append(td2)
+            tr.append(td3)
+            tr.append(tdCount)
+            tr.append(tdId)
+            tr.append(td4)
+        
+            if (tBody) {
+                
+                tBody.append(tr)
+
+            }
+            btn.addEventListener("click",function(e){
+                let basket=JSON.parse(localStorage.getItem("basket"))
+                let res=basket.find((val)=>{
+                  return  val.id==e.target.parentElement.parentElement.querySelector("#id").innerText
+                })
+                
+
+                let map=basket.map((val)=>{
+                    if (val.id!=res.id) {
+                        return val
+                    }
+                })
+                console.log(map);
+
+                
+               let aaa= map.filter((val)=>{
+                return val;
+               })
+                console.log(aaa);
+                localStorage.setItem("basket",JSON.stringify(aaa))
+                e.target.parentElement.parentElement.remove();
+                
+                if (tBody) {
+                    if (!tBody.firstElementChild) {
+                        tableZone.style.display="none"
+                        priceZone.style.display="none"
+                        empty.style.display="block"
+                        empty.style.textAlign="center"
+                    }
+                    else{
+                        empty.style.display="none"
+                    }
+                }
+            })
+            
+        })
+        
+        
+        // 
+        
+}
+window.addEventListener("DOMContentLoaded",function(){
+    if (tBody) {
+        if (!tBody.firstElementChild) {
+            tableZone.style.display="none"
+            priceZone.style.display="none"
+            empty.style.display="block"
+            empty.style.textAlign="center"
+        }
+        else{
+            empty.style.display="none"
+        }
+    }
+    
+})
+AddToCard()
